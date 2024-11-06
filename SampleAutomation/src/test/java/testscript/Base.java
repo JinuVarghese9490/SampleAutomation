f0d2ecf47cfd1629d8b393e54508e821f8c9dccc
JinuVarghese9490 <varghesejinu895@gmail.com>
2024-11-06 16:19:06 +0530

    first commit

---
+++ SampleAutomation/src/test/java/testscript/Base.java
@@ -0,0 +1,32 @@
+package testscript;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Base {
+	
+public WebDriver driver;
+	
+	public void initialiseBrowser()
+	{
+		driver=new ChromeDriver();
+		driver.get("https://selenium.qabible.in/simple-form-demo.php");
+		driver.manage().window().maximize();
+		
+	}
+	
+	public void driverQuit()
+	{
+		driver.quit();
+	}
+	
+
+	public static void main(String[] args)
+	{
+		
+		Base base=new Base();
+		base.initialiseBrowser();
+		base.driverQuit();
+	}
+
+}
